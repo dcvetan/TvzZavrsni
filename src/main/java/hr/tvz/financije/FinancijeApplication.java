d91be5b93e3625743f2b0f7149d4ce08579c1812
Dominik Cvetan <dominik.cvetan@gmail.com>
2023-07-25 19:08:56 +0200

    initial commit

---
+++ src/main/java/hr/tvz/financije/FinancijeApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.financije;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancijeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancijeApplication.class, args);
+    }
+
+}
