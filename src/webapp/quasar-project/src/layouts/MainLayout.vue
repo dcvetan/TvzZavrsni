53302d3f379351784d20fecacf708483c1e2953a
Dominik Cvetan <dominik.cvetan@gmail.com>
2023-08-09 21:05:58 +0200

    init webapp

---
+++ src/webapp/quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Personal finance
+        </q-item-label>
+
+<!--        <q-item-->
+<!--          clickable-->
+<!--          :to="{name:''}"-->
+<!--        >-->
+<!--          <q-item-section-->
+<!--            avatar-->
+<!--          >-->
+<!--            <q-icon name="" />-->
+<!--          </q-item-section>-->
+
+<!--          <q-item-section>-->
+<!--            <q-item-label></q-item-label>-->
+<!--            <q-item-label caption></q-item-label>-->
+<!--          </q-item-section>-->
+<!--        </q-item>-->
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
